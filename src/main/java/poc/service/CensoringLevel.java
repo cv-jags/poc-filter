5b30c111e7fa2e6f55abb5b86d40abe27060b7e4
Jose Angel Garcia <cv.jags@gmail.com>
2018-06-10 17:22:17 +0200

    Adds censoring level as enum.

---
+++ src/main/java/poc/service/CensoringLevel.java
@@ -0,0 +1,14 @@
+/**
+ * POC for Starz Play Arabia (June 2018)
+ */
+package poc.service;
+
+/**
+ * 
+ * @author <cv.jags@gmail.com>
+ *
+ */
+public enum CensoringLevel {
+	censored,
+	uncensored
+}
