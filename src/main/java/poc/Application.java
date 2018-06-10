f5ea0a63fd11e56a867349613735ef1dbcf73da6
Jose Angel Garcia <cv.jags@gmail.com>
2018-06-10 17:17:11 +0200

    Registers error handler in restTemplate.

--- src/main/java/poc/Application.java
+++ src/main/java/poc/Application.java
@@ -5,6 +5,13 @@ package poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import poc.exception.BypassErrorHandler;
 
 /**
  *
@@ -14,7 +21,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Configuration
+  public class AppConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder, BypassErrorHandler errorHandler) {
+      return builder.requestFactory(OkHttp3ClientHttpRequestFactory.class).errorHandler(errorHandler).build();
+    }
+  }
 }
